6c071021f80d601ac314aa50a8fe66e0268e9421
FrankDella <francescodm200218@gmail.com>
2025-06-29 22:11:08 +0200

    Correzione gestione errore nelle funzioni 'analyze_page' e 'analyle_api'

--- app/app.py
+++ app/app.py
@@ -40,7 +40,7 @@ def analyze_page():
             text, status_code = results
             return render_template('error.html', error=text), status_code
         
-    except prawcore.exceptions.PRAWException|prawcore.exceptions.Redirect:
+    except prawcore.exceptions.Redirect:
         return render_template('error.html', error='Subreddit non trovato'), 404
 
     except prawcore.exceptions.Forbidden:
@@ -99,7 +99,7 @@ def analyze_api():
             return jsonify({'error': text}), status_code
         return jsonify({k.isoformat(): v for k, v in results.items()})
 
-    except prawcore.exceptions.PRAWException|prawcore.exceptions.Redirect:
+    except prawcore.exceptions.Redirect:
         return jsonify({'error': 'Subreddit non trovato'}), 404
 
     except prawcore.exceptions.Forbidden:
